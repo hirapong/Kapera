0b6fd7ed5ec01648113061053b7649e69d5214bf
nori0620 <nori0620@gmail.com>
2011-08-07 11:48:33 +0900

    commit wasure

--- gen/com/nori0620/kapera/R.java
+++ gen/com/nori0620/kapera/R.java
@@ -12,6 +12,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int icon_kapera=0x7f020001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -19,5 +20,6 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int main_title=0x7f040002;
     }
 }
--- src/com/nori0620/kapera/KaperaActivity.java
+++ src/com/nori0620/kapera/KaperaActivity.java
@@ -3,19 +3,24 @@ package com.nori0620.kapera;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
 
 public class KaperaActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+    	setTitle( R.string.main_title );
+    	getWindow().requestFeature(Window.FEATURE_LEFT_ICON);
         setContentView(R.layout.main);
+    	getWindow().setFeatureDrawableResource( Window.FEATURE_LEFT_ICON, R.drawable.icon );
     }
     
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d( "Kapera", "hello!! new pause" );
+        Log.d( "Kapera", "hello!! new paaause" );
     }
     
     
